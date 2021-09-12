5bdd3cdc8abfb56e7ea7f5d4431cf7a8f026e2c6
giovannicampa <giocampa93@gmail.com>
2021-09-12 11:14:21 +0200

    Threshold and Voxel filter

--- catkin_ws/src/point_cloud_processing/src/pcl_preprocessing.cpp
+++ catkin_ws/src/point_cloud_processing/src/pcl_preprocessing.cpp
@@ -7,26 +7,64 @@
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 
-class PCLSegmentation {       // The class
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+
+#include <pcl_ros/point_cloud.h>
+
+class PCLSegmentation
+{ // The class
 
 private:
     ros::NodeHandle nh_;
     ros::Subscriber sub_;
-    ros::Publisher pub_; 
+    ros::Publisher pub_;
 
 public:
-
     // Constructor
-    PCLSegmentation() { 
-        sub_ = nh_.subscribe<pcl::PointCloud<pcl::PointXYZRGB> > ("/camera/camera_modelet_manager/camera/depth_registered/points", 5, &PCLSegmentation::callback, this);
-        pub_ = nh_.advertise<sensor_msgs::PointCloud2> ("pcl_cluster", 500);
+    PCLSegmentation()
+    {
+        sub_ = nh_.subscribe<sensor_msgs::PointCloud2>("velodyne_points", 5, &PCLSegmentation::callback, this);
+        pub_ = nh_.advertise<sensor_msgs::PointCloud2>("pcl_cluster", 500);
     }
 
     // Subscriber callback
-    void callback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg)
+    void callback(const sensor_msgs::PointCloud2ConstPtr &msg)
     {
-    }
 
+        // Convert ROS msg PointCloud2 type to PCL PointCloud2 type
+        pcl::PCLPointCloud2 pointCloud2;
+        pcl_conversions::toPCL(*msg, pointCloud2);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr PointCloud(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::fromPCLPointCloud2(pointCloud2, *PointCloud);
+
+
+        sensor_msgs::PointCloud2::Ptr passthrough_filtered (new sensor_msgs::PointCloud2);
+
+        pcl::PassThrough<pcl::PointXYZ> passThrough;
+        passThrough.setInputCloud(PointCloud);
+        passThrough.setFilterFieldName("z");
+        passThrough.setFilterLimits(-0.3, 2);
+        passThrough.filter(*PointCloud);
+
+        pcl::VoxelGrid<pcl::PointXYZ> sor;
+        sor.setInputCloud(PointCloud);
+        sor.setLeafSize(0.1,0.1,0.1);
+        sor.filter(*PointCloud);
+
+
+        pcl::PCLPointCloud2 pcl_temp;
+        pcl::toPCLPointCloud2(*PointCloud, pcl_temp);
+
+        // Convert PCL PointCloud2 to ROS msg PointCloud2 type
+        sensor_msgs::PointCloud2 PointCloud_AfterVoxelGrid;
+        pcl_conversions::fromPCL(pcl_temp, PointCloud_AfterVoxelGrid);
+        PointCloud_AfterVoxelGrid.header = msg->header;
+
+
+        pub_.publish(PointCloud_AfterVoxelGrid);
+
+    }
 };
 
 int main(int argc, char **argv)
